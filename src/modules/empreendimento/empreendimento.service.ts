b74ff9b7c0267099d405eb8b876f662699412401
Leonardo Arena <leonardoarena7@gmail.com>
2026-03-07 16:55:26 -0300

    refactor: Adiciona tipagem em `empreendimento.service.ts`

--- src/modules/empreendimento/empreendimento.service.ts
+++ src/modules/empreendimento/empreendimento.service.ts
@@ -1,26 +1,32 @@
 import { prisma } from "../../db.js";
+import type { Empreendimento, Prisma } from "@prisma/client";
 
-export function list() {
+export function list(): Promise<Empreendimento[]> {
   return prisma.empreendimento.findMany();
 }
 
-export function getById(id: string) {
+export function getById(id: string): Promise<Empreendimento | null> {
   return prisma.empreendimento.findUnique({
     where: { id },
   });
 }
 
-export function create(data: any) {
+export function create(
+  data: Prisma.EmpreendimentoCreateInput,
+): Promise<Empreendimento> {
   return prisma.empreendimento.create({ data });
 }
 
-export function update(id: string, data: any) {
+export function update(
+  id: string,
+  data: Prisma.EmpreendimentoUpdateInput,
+): Promise<Empreendimento> {
   return prisma.empreendimento.update({
     where: { id },
     data,
   });
 }
 
-export function remove(id: string) {
+export function remove(id: string): Promise<Empreendimento> {
   return prisma.empreendimento.delete({ where: { id } });
 }
