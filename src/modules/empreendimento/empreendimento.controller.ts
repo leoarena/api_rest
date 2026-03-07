995b36a056484f381d942ed38ddb54c13cde2f4e
Leonardo Arena <leonardoarena7@gmail.com>
2026-03-07 17:06:51 -0300

    refactor: Adiciona tipagem em `empreendimento.controller.ts`

--- src/modules/empreendimento/empreendimento.controller.ts
+++ src/modules/empreendimento/empreendimento.controller.ts
@@ -1,19 +1,26 @@
 import { Request, Response, NextFunction } from "express";
 import * as service from "./empreendimento.service.js";
+import { Empreendimento, Prisma } from "@prisma/client";
+
+type IdParam = { id: string };
 
 export async function list(req: Request, res: Response, next: NextFunction) {
   try {
-    const empreendimentos = await service.list();
+    const empreendimentos: Empreendimento[] = await service.list();
     res.json(empreendimentos);
   } catch (error) {
-    next(Error);
+    next(error);
   }
 }
 
-export async function getById(req: Request, res: Response, next: NextFunction) {
+export async function getById(
+  req: Request<IdParam>,
+  res: Response,
+  next: NextFunction,
+) {
   try {
     const { id } = req.params;
-    const empreendimento = await service.getById(id);
+    const empreendimento: Empreendimento | null = await service.getById(id);
     if (!empreendimento) {
       return res.status(404).json({ error: "Empreendimento não encontrado" });
     }
@@ -23,26 +30,38 @@ export async function getById(req: Request, res: Response, next: NextFunction) {
   }
 }
 
-export async function create(req: Request, res: Response, next: NextFunction) {
+export async function create(
+  req: Request<{}, {}, Prisma.EmpreendimentoCreateInput>,
+  res: Response,
+  next: NextFunction,
+) {
   try {
-    const empreendimento = await service.create(req.body);
+    const empreendimento: Empreendimento = await service.create(req.body);
     res.status(201).json(empreendimento);
   } catch (error) {
     next(error);
   }
 }
 
-export async function update(req: Request, res: Response, next: NextFunction) {
+export async function update(
+  req: Request<IdParam, {}, Prisma.EmpreendimentoUpdateInput>,
+  res: Response,
+  next: NextFunction,
+) {
   try {
     const { id } = req.params;
-    const empreendimento = await service.update(id, req.body);
+    const empreendimento: Empreendimento = await service.update(id, req.body);
     res.json(empreendimento);
   } catch (error) {
     next(error);
   }
 }
 
-export async function remove(req: Request, res: Response, next: NextFunction) {
+export async function remove(
+  req: Request<IdParam>,
+  res: Response,
+  next: NextFunction,
+) {
   try {
     const { id } = req.params;
     await service.remove(id);
